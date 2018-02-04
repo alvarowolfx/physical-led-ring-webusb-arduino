63852280df0fc2f4e48932fb2c0fc58ef0db0d12
Alvaro Viebrantz <alvarowolfx@gmail.com>
2018-02-04 17:34:22 -0300

    [feat] Add hwid for Arduino Micro

--- webapp/src/Serial.js
+++ webapp/src/Serial.js
@@ -6,6 +6,7 @@ export function getPorts() {
 
 export function requestPort() {
   const filters = [
+    { vendorId: 0x2341, productId: 0x0037 },
     { vendorId: 0x2341, productId: 0x8036 },
     { vendorId: 0x2341, productId: 0x8037 },
     { vendorId: 0x2341, productId: 0x804d },
